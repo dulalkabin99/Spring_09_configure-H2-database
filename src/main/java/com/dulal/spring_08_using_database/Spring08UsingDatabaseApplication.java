76414c01b65f9b754a04273ea6159c972bac2a95
Kabin Dulal <dulalkabin99@gmail.com>
2018-06-28 10:26:54 -0400

    initial commit

---
+++ src/main/java/com/dulal/spring_08_using_database/Spring08UsingDatabaseApplication.java
@@ -0,0 +1,12 @@
+package com.dulal.spring_08_using_database;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Spring08UsingDatabaseApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Spring08UsingDatabaseApplication.class, args);
+	}
+}
---
+++ src/test/java/com/dulal/spring_08_using_database/Spring08UsingDatabaseApplicationTests.java
@@ -0,0 +1,16 @@
+package com.dulal.spring_08_using_database;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class Spring08UsingDatabaseApplicationTests {
+
+	@Test
+	public void contextLoads() {
+	}
+
+}
